bb2d980f66e444f946df66191f37c705f84ba70b
Henrique Schell <henrique.schell@gmail.com>
2025-07-02 14:51:40 -0300

    finished project

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+/*1. Quando o botão for clicado
+2. Pegue o valor do input de e-mail
+3. Veja se esse valor inclui "@"
+4. Se não incluir, mostre erro
+5. Se incluir, aceite o formulário*/
+
+//get values
+var btn = document.getElementById('btn')
+var email = document.getElementById('email')
+var errorMessage = document.getElementById('errorMessage')
+var hiddenMain = document.getElementById('hidden-main')
+var thanks = document.getElementById('thanks')
+var btnDismiss = document.getElementById('btn-dismiss')
+
+//validation
+btn.addEventListener('click', () => {
+    const emailValue = email.value 
+    if (!emailValue.includes('@')){
+        email.classList.add('bg-red-200', 'text-red-600', 'ring', 'focus:ring-red-600')
+        email.classList.remove('focus:ring-[#242742]')
+        errorMessage.classList.remove('hidden')
+    }else{
+        hiddenMain.classList.add('hidden')
+        thanks.classList.remove('hidden')
+    }
+})
+
+btnDismiss.addEventListener('click', () => {
+    hiddenMain.classList.remove('hidden')
+    thanks.classList.add('hidden')
+})
+
+
+
+
+
